d0936be5d0d8bf9cac5fb8a61dfdc4f99e06749d
salahmohsen <salahmmohsen@gmail.com>
2024-01-03 06:30:17 +0200

    transactions design

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -6,7 +6,7 @@ import Transactions from "../ui/dashboard/transactions/transactions";
 const Dashboard = () => {
   return (
     <div className="flex gap-5 mt-5">
-      <div className="flex-1 flex-col">
+      <div className="flex-1 flex-col gap-10 flex">
         <div className="flex flex-row justify-between gap-5">
           <Card />
           <Card />
@@ -15,7 +15,7 @@ const Dashboard = () => {
         <Transactions />
         <Chart />
       </div>
-      <div className=" min-w-60">
+      <div className=" min-w-44">
         <RightBar />
       </div>
     </div>
--- app/ui/dashboard/transactions/transactions.jsx
+++ app/ui/dashboard/transactions/transactions.jsx
@@ -1,5 +1,111 @@
+import Image from "next/image";
+
 const Transactions = () => {
-  return <div>Transactions</div>;
+  return (
+    <div className="flex gap-5 flex-col ">
+      <h2 className="font-semibold">Latst Transactions</h2>
+      <table className="text-left bg-bgSoft shadow-sm rounded-lg divide-y divide-bg">
+        <thead>
+          <tr>
+            <th className="px-6 py-3">Name</th>
+            <th className="px-6 py-3">Status</th>
+            <th className="px-6 py-3">Date</th>
+            <th className="px-6 py-3">Amount</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td className="flex items-center gap-2 px-6 py-3">
+              <Image
+                src="/noavatar.png"
+                alt="avatar"
+                width={40}
+                height={40}
+                className="rounded-full bg-cover"
+              />
+              <span>Salah Mohsen</span>
+            </td>
+            <td className="px-6 py-3">
+              <span>Pending</span>
+            </td>
+            <td className="px-6 py-3">03.01.2024</td>
+            <td className="px-6 py-3">$3.20</td>
+          </tr>
+          <tr>
+            <td className="flex items-center gap-2 px-6 py-3">
+              <Image
+                src="/noavatar.png"
+                alt="avatar"
+                width={40}
+                height={40}
+                className="rounded-full bg-cover"
+              />
+              <span>Salah Mohsen</span>
+            </td>
+            <td className="px-6 py-3">
+              <span>Pending</span>
+            </td>
+            <td className="px-6 py-3">03.01.2024</td>
+            <td className="px-6 py-3">$3.20</td>
+          </tr>
+
+          <tr>
+            <td className="flex items-center gap-2 px-6 py-3">
+              <Image
+                src="/noavatar.png"
+                alt="avatar"
+                width={40}
+                height={40}
+                className="rounded-full bg-cover"
+              />
+              <span>Salah Mohsen</span>
+            </td>
+            <td className="px-6 py-3">
+              <span>Pending</span>
+            </td>
+            <td className="px-6 py-3">03.01.2024</td>
+            <td className="px-6 py-3">$3.20</td>
+          </tr>
+
+          <tr>
+            <td className="flex items-center gap-2 px-6 py-3">
+              <Image
+                src="/noavatar.png"
+                alt="avatar"
+                width={40}
+                height={40}
+                className="rounded-full bg-cover"
+              />
+              <span>Salah Mohsen</span>
+            </td>
+            <td className="px-6 py-3">
+              <span>Pending</span>
+            </td>
+            <td className="px-6 py-3">03.01.2024</td>
+            <td className="px-6 py-3">$3.20</td>
+          </tr>
+
+          <tr>
+            <td className="flex items-center gap-2 px-6 py-3">
+              <Image
+                src="/noavatar.png"
+                alt="avatar"
+                width={40}
+                height={40}
+                className="rounded-full bg-cover"
+              />
+              <span>Salah Mohsen</span>
+            </td>
+            <td className="px-6 py-3">
+              <span>Pending</span>
+            </td>
+            <td className="px-6 py-3">03.01.2024</td>
+            <td className="px-6 py-3">$3.20</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default Transactions;
